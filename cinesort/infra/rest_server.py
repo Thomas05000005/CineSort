242cf33940401bd96eef9a355cbbf5754cb8dd7a
Thomas Blanc <thomas@exemple.com>
2026-06-09 01:57:56 +0200

    fix(csp): autoriser image.tmdb.org dans img-src (3 emissions)

--- cinesort/infra/rest_server.py
+++ cinesort/infra/rest_server.py
@@ -718,7 +718,7 @@ class _CineSortHandler(BaseHTTPRequestHandler):
                 "script-src 'self'; "
                 "style-src 'self' 'unsafe-inline'; "
                 "font-src 'self'; "
-                "img-src 'self' data:; "
+                "img-src 'self' data: https://image.tmdb.org; "
                 "connect-src 'self'; "
                 "frame-ancestors 'none'; "
                 "base-uri 'self'",
@@ -732,7 +732,7 @@ class _CineSortHandler(BaseHTTPRequestHandler):
                 "script-src 'self'; "
                 "style-src 'self'; "
                 "font-src 'self'; "
-                "img-src 'self' data:; "
+                "img-src 'self' data: https://image.tmdb.org; "
                 "connect-src 'self'; "
                 "frame-ancestors 'none'; "
                 "base-uri 'self'",
